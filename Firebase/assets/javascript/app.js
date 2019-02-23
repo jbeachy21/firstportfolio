2ca99fad22221d6bab46e0c59dc0c205683aff32
Jasper Beachy <jbeachy@gustavus.edu>
2019-02-23 10:25:33 -0600

    firebase changes made

--- Firebase/assets/javascript/app.js
+++ Firebase/assets/javascript/app.js
@@ -37,25 +37,6 @@ $(document).ready(function () {
     
   })
   
-  //var current_time = new moment().format("HH:mm");
-  //console.log(current_time);
-
-  var whenTheTrainFirstArrives = moment("13:00", "HH:mm");
-  var frequency = 25;
-  var rightNow = moment("16:19", "HH:mm");
-  //1:00 -> 1:25 -> 1:50 -> 2:15 -> 2:40 -> 3:05 -> 3:30 -> 3:55 -> 4:20
-  //minutesAway = 20
-  var timeDiff = whenTheTrainFirstArrives.diff(rightNow, 'minutes');
-  if(timeDiff < 0) {
-    timeDiff = timeDiff * -1;
-  }
-  console.log(timeDiff);
-  var offSet = timeDiff % frequency;
-  console.log(offSet);
-
-  var timeTillTrain = frequency - offSet;
-  console.log(timeTillTrain);
-  
   
 
   $("#submit").on ("click", function(event) {
@@ -115,6 +96,42 @@ $(document).ready(function () {
       $("<td>").text(TrainTime))
       // $("<td>").text(Tminutes);
     $("#train-table > tbody").append(newRow);
+
+
+
+
+
+  //var current_time = new moment().format("HH:mm");
+  //console.log(current_time);
+
+  // var whenTheTrainFirstArrives = moment("13:00", "HH:mm");
+  // var frequency = 25;
+  // var rightNow = moment("16:19", "HH:mm");
+  // //1:00 -> 1:25 -> 1:50 -> 2:15 -> 2:40 -> 3:05 -> 3:30 -> 3:55 -> 4:20
+  // //minutesAway = 20
+  // var timeDiff = whenTheTrainFirstArrives.diff(rightNow, 'minutes');
+  // if(timeDiff < 0) {
+  //   timeDiff = timeDiff * -1;
+  // }
+  // console.log("timediff: " + timeDiff);
+  // var offSet = timeDiff % frequency;
+  // console.log("offSet: " + offSet);
+
+  // var timeTillTrain = frequency - offSet;
+  // console.log("timeTillTrain: " + timeTillTrain);
+
+  var whenTheTrainFirstArrives = TrainTime;
+  console.log("whenTheTrainFirstArrives: " + whenTheTrainFirstArrives);
+
+  var frequency = TrainFrequency;
+  console.log("TrainFrequency: " + frequency);
+
+  var rightNow = moment().format('HH:mm');
+  console.log("rightNow: " + rightNow);
+
+  console.log("\n");
+
+  
     
   })
 });
